a3bbc51d798ea14be742bc866d32f6b47f2e88f5
Kr3ing <krishnahmunalem.lorejo@my.smciligan.edu.ph>
2024-12-28 19:07:29 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { View, Image, Text, TouchableOpacity, StyleSheet, ImageBackground, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 function Main({ navigation }) {
   return (
@@ -7,8 +9,8 @@ function Main({ navigation }) {
       source={require('./images/login-bg.png')} 
       style={styles.background}
     >
-              <Image source={require('./images/smclogo.png')} style={styles.logo} pointerEvents="none" />
       <View style={styles.container}>
+        <Image source={require('./images/smclogo.png')} style={styles.logo} pointerEvents="none" />
         <Text style={styles.title}>
           Welcome to St. Michael’s{'\n'}College Alumni Yearbook!
         </Text>
@@ -32,48 +34,45 @@ const styles = StyleSheet.create({
   },
 
   container: {
-    top: 295,
+    marginTop: -60,
     flex: 1,
-    backgroundColor: '#24348E', 
-    width: 360,
-    paddingVertical: 20,
+    width: '90%', 
+    paddingVertical: height * 0.05, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
   },
 
   logo: {
-    width: 220,
-    height: 220,
-    top: 150,
+    width: width * 0.6, 
+    height: width * 0.6, 
     opacity: 0.9,
+    marginBottom: 90,
   },
 
   title: {
     color: 'white',
-    fontSize: 23,
+    fontSize: width * 0.06, 
     textAlign: 'center',
     fontWeight: '500',
-    marginVertical: 20,
-    marginTop: -90,
-    marginBottom: -50,
+    marginVertical: height * 0.02, 
   },
 
   button: {
-    zIndex: 1,
-    paddingVertical: 6,
-    paddingHorizontal: 0,
+    paddingVertical: height * 0.01, 
     backgroundColor: 'white',
     borderRadius: 25,
-    marginTop: 20,
+    marginTop: height * 0.02, 
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    width: 250,
-    marginLeft: 55,
+    width: '80%',
+    alignSelf: 'center', 
   },
 
   buttonText: {
-    fontSize: 20,
+    fontSize: width * 0.05, 
     fontWeight: 'bold',
     color: '#3b9eff',
     textAlign: 'center',
@@ -84,8 +83,7 @@ const styles = StyleSheet.create({
     height: 1,
     backgroundColor: '#ddd',
     alignSelf: 'center',
-    marginVertical: 15,
-    marginTop: 85,
+    marginVertical: height * 0.02, 
   },
 });
 
